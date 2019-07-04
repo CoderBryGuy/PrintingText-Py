eaef4fdfaf14e4064baca5a332caa499ac8566cb
Bryan <bryan.dov.bergman@gmail.com>
2019-07-04 20:03:25 +0300

    Initial commit

---
+++ PrintExercise.py
@@ -0,0 +1,29 @@
+_author_ = "dev"
+
+# print("my hovercraft is full of eels")
+#
+# print(6*7)
+#
+# greeting = "hello"
+#
+# #name = input(" please enter your name")
+# #print(greeting + ' ' + name)
+#
+# splitString = " this string has been \n split over \n several \n lines"
+# print(splitString)
+#
+# tabbedString = "1\t2\t3\t4\t5\t"
+# print(tabbedString)
+
+print('the pet shop owner said "no, no, \'e\'s uh, ...he\'s resting"' )
+print("the pet shop owner said \"no, no, 'e's uh, ...he's resting\"" )
+
+anotherSplitString = """This string has been
+split over
+several lines"""
+
+print(anotherSplitString)
+
+print('''the pet shop owner said "no, no, 'e's uh, ...he's resting"''' )
+print("""the pet shop owner said "no, no, 'e's uh, ...he's resting" """ )
+
